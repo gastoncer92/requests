2e5ec8d4a582defe0486f98245edc162333a9de8
gastoncer92 <kaupisoft@gmail.com>
2022-12-22 14:57:11 -0300

    guardado2

--- funciones.py
+++ funciones.py
@@ -73,15 +73,46 @@ def extraer(url, excluir, patron):
     if existeUrl(url):
         extracto = re.findall(patron, respuesta.text)
         for index, i in enumerate(extracto):
-            for exc in excluir:
-                if exc in i:
-                    registroReverso.append(i)
-                else:
-                    pass
-        res = [i for i in extracto if i not in registroReverso]
-        return res
+            if excluir!=['']:
+                for exc in excluir:
+                    if exc in i:
+                        registroReverso.append(i)
+                    else:
+                        pass
+                res = [i for i in extracto if i not in registroReverso]
+                return res
+            else:
+                return extracto
     else:
         return None
+def extraer_sin_direccionamiento(url, excluir, patron):
+    registroReverso = []
+    respuesta = requests.get(url)
+    if existeUrl(url):
+        extracto = re.findall(patron, respuesta.text)
+        for index, i in enumerate(extracto):
+            if excluir!=['']:
+                for exc in excluir:
+                    if exc in i:
+                        registroReverso.append(i)
+                    else:
+                        pass
+                res = [i for i in extracto if i not in registroReverso]
+                return res
+            else:
+                return extracto
+    else:
+        return None
+
+def extraer_sin_excluir(url,patron):
+
+    respuesta = requests.get(url)
+    if existeUrl(url):
+        extracto = re.findall(patron, respuesta.text)
+        return extracto
+    else:
+        return None
+
 
 
 def existeUrl(url):
@@ -91,6 +122,58 @@ def existeUrl(url):
         return False
 
 
+def ListaDeCorreos2(urls=[''], direccionamiento=[''], excluir=[''], patron=r'[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}'):
+    """
+    Retorna una lista de correos extridos de la lista de urls administrada
+
+    :param urls: list
+    :param direccionamiento: list
+    :param excluir: list
+    :param patron: str
+    :return: list
+    """
+    ## preparo una lista vacía en la que iran todos los correos
+    todasLasUrl = []
+    # ver si direccionamiento y excluir tienen contenido
+    if direccionamiento != [''] and excluir != ['']:
+        ## tienen ambos contenido
+        ## meto en la lista todasLasUrl las url originales
+        for i in urls:
+            todasLasUrl.append(i)
+        ## recorro las direcciones para agregarla a las urls administradas
+        for t in direccionamiento:
+            ## recorro las url y les agrego nuevas url con las direcciones extras
+            for i in urls:
+                todasLasUrl.append(i + '/' + t)
+        print(todasLasUrl)
+        ## retorno todas las urls preparadas
+        return todasLasUrl
+
+
+    elif direccionamiento == [''] and excluir == ['']:
+        print("no hay direccionamiento ni excluir!")
+
+        for i in urls:
+            todasLasUrl.append(i)
+        return todasLasUrl
+    else:
+        print("no hay direccionamiento o no hay excluir!!")
+        if direccionamiento == ['']:
+            print("no hay direccionamiento")
+
+
+            for i in urls:
+                todasLasUrl.append(i)
+            return todasLasUrl
+
+        else:
+            print("no hay excluir")
+            for i in urls:
+                todasLasUrl.append(i)
+            for t in direccionamiento:
+                for i in urls:
+                    todasLasUrl.append(i + '/' + t)
+            return todasLasUrl
 def ListaDeCorreos(urls=[''], direccionamiento=[''], excluir=[''], patron=r'[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}'):
     """
     Retorna una lista de correos extridos de la lista de urls administrada
@@ -106,7 +189,6 @@ def ListaDeCorreos(urls=[''], direccionamiento=[''], excluir=[''], patron=r'[a-z
     # ver si direccionamiento y excluir tienen contenido
     if direccionamiento != [''] and excluir != ['']:
         ## tienen ambos contenido
-
         ## meto en la lista todasLasUrl las url originales
         for i in urls:
             todasLasUrl.append(i)
@@ -115,35 +197,32 @@ def ListaDeCorreos(urls=[''], direccionamiento=[''], excluir=[''], patron=r'[a-z
             ## recorro las url y les agrego nuevas url con las direcciones extras
             for i in urls:
                 todasLasUrl.append(i + '/' + t)
-
+        print(todasLasUrl)
         ## retorno todas las urls preparadas
         return todasLasUrl
 
+
     elif direccionamiento == [''] and excluir == ['']:
-        # print("no hay direccionamiento ni excluir!")
+        print("no hay direccionamiento ni excluir!")
 
         for i in urls:
             todasLasUrl.append(i)
         return todasLasUrl
     else:
-        # print("no hay direccionamiento o no hay excluir!!")
+        print("no hay direccionamiento o no hay excluir!!")
         if direccionamiento == ['']:
-            # print("no hay direccionamiento")
+            print("no hay direccionamiento")
+
 
             for i in urls:
                 todasLasUrl.append(i)
             return todasLasUrl
 
         else:
-            # print("no hay excluir")
-            ## meto en la lista todasLasUrl las url originales
+            print("no hay excluir")
             for i in urls:
                 todasLasUrl.append(i)
-            ## recorro las direcciones para agregarla a las urls administradas
             for t in direccionamiento:
-                ## recorro las url y les agrego nuevas url con las direcciones extras
                 for i in urls:
                     todasLasUrl.append(i + '/' + t)
-
-            ## retorno todas las urls preparadas
             return todasLasUrl
--- main.py
+++ main.py
@@ -22,6 +22,44 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         pc.copy(texto)
         self.statusbar.showMessage("texto copiado!", 3000)
 
+    def extraer(self):
+        # limpiar plainTextEdit_2
+        self.plainTextEdit_2.clear()
+
+        # preparar listas de urls
+        urls = self.plainTextEdit.toPlainText().strip().replace('\n', ',').split(',')
+        # preparar listas de exluir y direcciones
+        excluir = self.lineEdit.text().strip().split(',')
+        print(excluir)
+        print(excluir)
+        # preparar listas de direcciones
+        direccionamiento = self.lineEdit_2.text().strip().split(',')
+
+        # preparo variable de la lista de correos en str
+        textoPlanoCorreo = ""
+        # preparo variable de la cantidad de correos para statusbar
+
+        # determino patros para filtrar los correos
+        patron = r'[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}'
+        # lista de urls
+        lista_de_urls = ListaDeCorreos(urls, direccionamiento, excluir, patron)
+        lista_de_correos = []
+        for i in lista_de_urls:
+            list_email = extraer(i, excluir, patron)
+
+            if list_email is not None:
+                for correo in list_email:
+                    if correo not in lista_de_correos:
+                        lista_de_correos.append(correo)
+                for correo in lista_de_correos:
+                    textoPlanoCorreo = textoPlanoCorreo + ',' + correo
+                self.plainTextEdit_2.setPlainText(textoPlanoCorreo[1:])
+                self.statusbar.showMessage(
+                    "Se obtuvieron {} correos de {} paginas web".format(len(lista_de_correos), len(lista_de_urls)))
+
+            else:
+                pass
+
     def extraer2(self):
         # limpiar plainTextEdit_2
         self.plainTextEdit_2.clear()
@@ -38,27 +76,75 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # preparo variable de la lista de correos en str
         textoPlanoCorreo = ""
         # preparo variable de la cantidad de correos para statusbar
-        cantidad_de_correos = 0
 
         # determino patros para filtrar los correos
         patron = r'[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}'
         # lista de urls
-        lista_de_urls = ListaDeCorreos(urls, direccionamiento, excluir, patron)
+        lista_de_urls = ListaDeCorreos2(urls, direccionamiento, excluir, patron)
         lista_de_correos = []
-        for i in lista_de_urls:
-            list_email = extraer(i, excluir, patron)
-            if list_email != None:
-                for correo in list_email:
-                    if correo not in lista_de_correos:
-                        lista_de_correos.append(correo)
-                for correo in lista_de_correos:
-                    textoPlanoCorreo = textoPlanoCorreo + ',' + correo
-                self.plainTextEdit_2.setPlainText(textoPlanoCorreo[1:])
-                self.statusbar.showMessage(
-                    "Se obtuvieron {} correos de {} paginas web".format(len(lista_de_correos), len(lista_de_urls)))
+        print("excluir: {}".format(excluir))
+        if excluir == [''] and direccionamiento != ['']:
+            print("no hay excluir")
+            for i in lista_de_urls:
+                list_email = extraer_sin_excluir(i, patron)
+                if list_email is not None:
+                    for correo in list_email:
+                        if correo not in lista_de_correos:
+                            lista_de_correos.append(correo)
+                    for correo in lista_de_correos:
+                        textoPlanoCorreo = textoPlanoCorreo + ',' + correo
+                    self.plainTextEdit_2.setPlainText(textoPlanoCorreo[1:])
+                    self.statusbar.showMessage(
+                        "Se obtuvieron {} correos de {} paginas web".format(len(lista_de_correos), len(lista_de_urls)))
+                else:
+                    pass
 
-            else:
-                pass
+        elif excluir == [''] and direccionamiento == ['']:
+            print("no hay NADA!")
+            for i in lista_de_urls:
+                list_email = extraer_sin_direccionamiento(i, excluir, patron)
+                print(list_email)
+                if list_email is not None:
+                    for correo in list_email:
+                        print(correo)
+                        if correo not in lista_de_correos:
+                            lista_de_correos.append(correo)
+                    for correo in lista_de_correos:
+                        textoPlanoCorreo = textoPlanoCorreo + ', ' + correo
+                        self.plainTextEdit_2.setPlainText(textoPlanoCorreo[1:])
+                        self.statusbar.showMessage(
+                            "Se obtuvieron {} correos de {} paginas web".format(len(lista_de_correos),
+                                                                                len(lista_de_urls)))
+
+                else:
+                    pass
+
+
+
+        else:
+            for i in lista_de_urls:
+                list_email = extraer(i, excluir, patron)
+                if list_email is not None:
+                    for correo in list_email:
+                        if correo not in lista_de_correos:
+                            lista_de_correos.append(correo)
+                    for correo in lista_de_correos:
+                        textoPlanoCorreo = textoPlanoCorreo + ',' + correo
+                    self.plainTextEdit_2.setPlainText(textoPlanoCorreo[1:])
+                    self.statusbar.showMessage(
+                        "Se obtuvieron {} correos de {} paginas web".format(len(lista_de_correos), len(lista_de_urls)))
+                else:
+                    pass
+
+        # if excluir == [''] and direccionamiento == ['']:
+        #     print('OOOOOOOOO')
+        # elif excluir == [''] and direccionamiento != ['']:
+        #     print('LLLLLLLLLLLLLLLLLLLLLLLLLLLLLL')
+        #
+        # elif excluir == [''] and direccionamiento != ['']:
+        #     print('HHHHHHHHHHHHHHHHHH')
+        # else:
+        #     print('¿¿¿¿¿¿')
 
 
 app = QApplication([])
